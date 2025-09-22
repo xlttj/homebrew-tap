f9f77a1023c001d41090c0987a6915e01dcc85d3
Axel Tietje <axel@tietje.eu>
2025-09-22 15:11:02 +0200

    release: v1.1.0

--- Formula/kprtfwd.rb
+++ Formula/kprtfwd.rb
@@ -1,17 +1,17 @@
 class Kprtfwd < Formula
   desc "Kubernetes port-forward multiplexer"
   homepage "https://github.com/xlttj/kprtfwd"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/xlttj/kprtfwd/releases/download/v1.0.0/kprtfwd-darwin-arm64"
-      sha256 "3bb9a90db8b382cf511b319a6b5a152da9429dbc222b7e5859fa88494ee163fa"
+      url "https://github.com/xlttj/kprtfwd/releases/download/v1.1.0/kprtfwd-darwin-arm64"
+      sha256 "1ee147034074d61c78dd3c28eca6bdcc2a3544e677580eacfb7f47d6f0138230"
     end
     on_intel do
-      url "https://github.com/xlttj/kprtfwd/releases/download/v1.0.0/kprtfwd-darwin-amd64"
-      sha256 "2c3d559bddf1588159df500d25648475a21d788a190f35cf34028e33d4799be6"
+      url "https://github.com/xlttj/kprtfwd/releases/download/v1.1.0/kprtfwd-darwin-amd64"
+      sha256 "b3761bbe0fba38a7c071ed1a237cc83bb3e93819eb617c8f397d3e10d21b8f17"
     end
   end
 
